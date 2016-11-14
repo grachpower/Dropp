2d50c418ab2b032b410f1d1607a3cd2bd1697d20
Artyom Andreev <amiscary@gmail.com>
2016-11-14 16:51:51 +0300

    preloader

--- dist/js/preloader.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  window.addEventListener("load", function () {
-      var pagePreloader  = document.getElementById('page-preloader');
-      pagePreloader.style.display = 'none';
-  })
-})();
