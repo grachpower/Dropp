10800ce3df9409524c23f8e53e4d66d86a2cd0f5
Artyom Andreev <amiscary@gmail.com>
2016-11-14 13:24:29 +0300

    guilded & gh pages

---
+++ docs/js/preloader.js
@@ -0,0 +1,6 @@
+(function () {
+  window.addEventListener("load", function () {
+      var pagePreloader  = document.getElementById('page-preloader');
+      pagePreloader.style.display = 'none';
+  })
+})();
